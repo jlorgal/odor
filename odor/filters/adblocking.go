db10d7a05d69c122fbcf6b2dbef7064888cc4b6a
lentregu <xixon2002@gmail.com>
2017-10-20 03:41:16 +0200

    feat: adblocking filter

---
+++ odor/filters/adblocking.go
@@ -0,0 +1,36 @@
+package filters
+
+import (
+	"net"
+
+	"github.com/jlorgal/odor/odor"
+)
+
+// AdBlocking filter.
+type AdBlocking struct {
+	blacklist []*net.IPNet
+}
+
+// NewAdBlocking creates a Malware filter
+func NewAdBlocking(config *odor.Config) (*AdBlocking, error) {
+	blacklist, err := odor.GetBlacklist("adBlocking", config)
+	return &AdBlocking{blacklist: blacklist}, err
+}
+
+// Request filters ingress packets.
+func (a *AdBlocking) Request(context *odor.Context) odor.FilterAction {
+	if context.Profile == nil || !context.Profile.AdBlocking {
+		return odor.Accept
+	}
+	if ipv4 := odor.GetIPv4Layer(context.Packet); ipv4 != nil {
+		if odor.IsBlacklistedIP(a.blacklist, ipv4.DstIP) {
+			return odor.Drop
+		}
+	}
+	return odor.Accept
+}
+
+// Response filters egress packets.
+func (m *Malware) Response(context *odor.Context) odor.FilterAction {
+	return odor.Accept
+}
